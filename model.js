50da790bbabc58b757abf549416387b6843a487d
Luisa Salcedo <profe.salcedo@gmail.com>
2017-07-21 13:55:21 -0500

    completed code for model.js and config.js

---
+++ model.js
@@ -0,0 +1,62 @@
+//data layer of the application
+const mongoose = require("mongoose"); //cooks my order. Mongo will be my single source of truth
+
+const cutSchema = mongoose.Schema({
+  style: String,
+  weight: Number
+});
+
+cutSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    style: this.style,
+    weight: this.weight
+  };
+};
+
+const orderSchema = mongoose.Schema({
+  delivery: Date,
+  price: Number,
+  //additionalInstructions: [String],
+  cutId: String,
+  userId: String,
+  quantity: Number
+});
+
+orderSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    delivery: this.delivery,
+    price: this.price,
+    cutId: this.cutId,
+    userId: this.userId,
+    quantity: this.quantity
+  };
+};
+
+// {
+//   delivery: new Date("March 4, 2017"),
+//   price: 1234, // $12.24 everything is handles in cents, format for user in the front end
+//   additionalInstructions: [
+//     "please drop it off at my house",
+//     "other information here"
+//   ]
+// }
+
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    email: this.email
+  };
+};
+
+const Cut = mongoose.model("Cut", cutSchema);
+const Order = mongoose.model("Order", orderSchema);
+const User = mocha.model("User", userSchema);
+
+module.exports = { Cut, Order, User };
--- server.js
+++ server.js
@@ -1,4 +1,35 @@
-const express = require("express");
+const express = require("express"); //how we make express available to application code
+const morgan = require("morgan"); //logging middleware
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
+
+app.use(morgan("dev")); //registering morgan as a middlware function
+app.use(bodyParser.json());
+// our app will use bodyParser to try to
+// parse JSON and/or URL encoded data from
+// request bodies. If you don't add
+// body parsing middleware, even if the raw
+// request contains, say, a JSON body,
+// `req.body` will be empty in the request handler
+
+//app.use(bodyParser.urlencoded({extended: true}));
+//this would be used if I had a from in my html
+
+mongoose.Promise = global.Promise;
+
+const { DATABASE_URL, PORT } = require("./config");
+// const config = require("./config");
+// const DATABASE_URL = config.DATABSE_URL;
+// const PORT = config.PORT;
+const { Cut, Order, User } = require("./models");
+app.get("/Cuts," (req, res) => {
+ Cut.find()
+  .exec()//tell mongo function to quick off and start running
+  .then(cuts => {
+    res.json(posts.map)
+  })
+});
+
 app.subscribe(express.static("public"));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080); //line commands the server to start listening for client request
