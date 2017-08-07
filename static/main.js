677437d990efc23dbb804495b15771d9b926efe5
Luisa Salcedo <profe.salcedo@gmail.com>
2017-08-07 18:49:49 -0500

    cleaned up code for Model section, added test server

--- static/main.js
+++ static/main.js
@@ -1,29 +1,14 @@
 //single source of truth-MODEL
-
+//Cleaning up model functions. Not calling them immediately
+//Checklist
+//delete end points will need id's
+//for post I need information of what I am creating. It does need the object/thing to create
+//PUT needs (combo of POST/DELETE) Need ID and object that I am modifying
 const state = {
   //local copy of API objects
-  orders: [
-    {
-      delivery: Date,
-      price: Number,
-      cutId: "string",
-      userId: "string",
-      quentity: Number
-    }
-  ], //will download the data from server
-
-  cuts: [
-    {
-      style: "string",
-      weight: Number
-    }
-  ],
-  users: [
-    {
-      email: { type: "String", unique: true },
-      password: { type: "string", unique: true }
-    }
-  ]
+  orders: [], //will download the data from server
+  cuts: [],
+  users: []
 };
 
 const getOrders = () => {
@@ -39,7 +24,6 @@ const getOrders = () => {
       state.orders = orders;
     });
 };
-getOrders();
 
 const getCuts = () => {
   return fetch("/cuts", {
@@ -53,7 +37,6 @@ const getCuts = () => {
       state.cuts = cuts;
     });
 };
-getCuts();
 
 const getUsers = () => {
   return fetch("/users", {
@@ -67,133 +50,132 @@ const getUsers = () => {
       state.users = users;
     });
 };
-getUsers();
 
-const postOrders = () => {
-  return fetch("/orders", {
-    method: "POST"
-  })
-    .then(res => {
-      return res.json();
-    })
-    .then(orders => {
-      console.log(orders);
-      state.orders = orders;
-    });
-};
-postOrders();
+// const postOrders = () => {
+//   return fetch("/orders", {
+//     method: "POST"
+//   })
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(orders => {
+//       console.log(orders);
+//       state.orders = orders;
+//     });
+// };
+// postOrders();
 
-const postCuts = () => {
-  return fetch("/cuts", {
-    method: "POST"
-  })
-    .then(res => {
-      return res.json();
-    })
-    .then(cuts => {
-      console.log(cuts);
-      state.cuts = cuts;
-    });
-};
-postCuts();
+// const postCuts = () => {
+//   return fetch("/cuts", {
+//     method: "POST"
+//   })
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(cuts => {
+//       console.log(cuts);
+//       state.cuts = cuts;
+//     });
+// };
+// postCuts();
 
-const postUsers = () => {
-  return fetch("/users", {
-    method: "POST"
-  })
-    .then(res => {
-      return res.json();
-    })
-    .then(users => {
-      console.log(users);
-      state.users = users;
-    });
-};
-postUsers();
+// const postUsers = () => {
+//   return fetch("/users", {
+//     method: "POST"
+//   })
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(users => {
+//       console.log(users);
+//       state.users = users;
+//     });
+// };
+// postUsers();
 
-const putOrders = () => {
-  return fetch("/orders", {
-    method: "PUT"
-  })
-    .then(res => {
-      return res.json();
-    })
-    .then(orders => {
-      console.log(orders);
-      state.orders = orders;
-    });
-};
-putOrders();
+// const putOrders = () => {
+//   return fetch("/orders", {
+//     method: "PUT"
+//   })
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(orders => {
+//       console.log(orders);
+//       state.orders = orders;
+//     });
+// };
+// putOrders();
 
-const putCuts = () => {
-  return fetch("/cuts", {
-    method: "PUT"
-  })
-    .then(res => {
-      return res.json();
-    })
-    .then(cuts => {
-      console.log(cuts);
-      state.cuts = cuts;
-    });
-};
-putCuts();
+// const putCuts = () => {
+//   return fetch("/cuts", {
+//     method: "PUT"
+//   })
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(cuts => {
+//       console.log(cuts);
+//       state.cuts = cuts;
+//     });
+// };
+// putCuts();
 
-const putUsers = () => {
-  return fetch("/users", {
-    method: "PUT"
-  })
-    .then(res => {
-      return res.json();
-    })
-    .then(users => {
-      console.log(users);
-      state.users = users;
-    });
-};
-putUsers();
+// const putUsers = () => {
+//   return fetch("/users", {
+//     method: "PUT"
+//   })
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(users => {
+//       console.log(users);
+//       state.users = users;
+//     });
+// };
+// putUsers();
 
-const deleteOrders = () => {
-  return fetch("/orders", {
-    method: "DELETE"
-  })
-    .then(res => {
-      return res.json();
-    })
-    .then(orders => {
-      console.log(orders);
-      state.orders = orders;
-    });
-};
-deleteOrders();
+// const deleteOrders = () => {
+//   return fetch("/orders", {
+//     method: "DELETE"
+//   })
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(orders => {
+//       console.log(orders);
+//       state.orders = orders;
+//     });
+// };
+// deleteOrders();
 
-const deleteCut = () => {
-  return fetch("/cut", {
-    method: "DELETE"
-  })
-    .then(res => {
-      return res.json();
-    })
-    .then(cut => {
-      console.log(cut);
-      state.cut = cut;
-    });
-};
-deleteCut();
+// const deleteCut = () => {
+//   return fetch("/cut", {
+//     method: "DELETE"
+//   })
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(cut => {
+//       console.log(cut);
+//       state.cut = cut;
+//     });
+// };
+// deleteCut();
 
-const deleteUser = () => {
-  return fetch("/users", {
-    method: "DELETE"
-  })
-    .then(res => {
-      return res.json();
-    })
-    .then(users => {
-      console.log(users);
-      state.users = users;
-    });
-};
-deleteUser();
+// const deleteUser = () => {
+//   return fetch("/users", {
+//     method: "DELETE"
+//   })
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(users => {
+//       console.log(users);
+//       state.users = users;
+//     });
+// };
+// deleteUser();
 
 /* global $ */
 //will help with making posts
@@ -217,13 +199,34 @@ deleteUser();
 // });
 
 //Rendering-VIEW
-const render = state => {
-  var createOrder = state.orders;
-};
+//Checklist
+//Render info per page
+//Set up page pick to show/hide current page
+//
+// const render = state => {
+//   var createOrder = state.orders;
+// };
 //build out the getCuts function, have data at state.cuts
+
 //ACTIONS: CONTROLER Things users can do.
+//User story
+//As user of Beef USA I want to log in with my email/password
+//Once I log in, greeting top of page
+//As user I want to create and order
+//As user I want to review past orders
+//As user I want to update current order
+//As an administrator I want to add price
+//As an administrator I want to add/delete cuts
+//As an administrator I want to grant/revoke access
+
 $(function() {
   const currentPage = {
-    currentPage: "homePage"
+    currentPage: "order-form"
   };
+  getCuts();
+  getOrders();
+  getUsers();
 });
+
+//create second landing page-Users
+//create third landing page-Administrators
